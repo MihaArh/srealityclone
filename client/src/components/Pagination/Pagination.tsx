511eb992381651289a8610347860d9dc4eaaa63f
Miha Arh <miha.arh06@gmail.com>
2023-10-05 14:54:17 +0200

    Change arrow symbols

--- client/src/components/Pagination/Pagination.tsx
+++ client/src/components/Pagination/Pagination.tsx
@@ -23,7 +23,7 @@ function Pagination({
           disabled={currentPage === 1}
           onClick={() => onPageChange(currentPage - 1)}
         >
-          &laquo;
+          &lsaquo;
         </button>
 
         <button
@@ -66,7 +66,7 @@ function Pagination({
           disabled={currentPage === totalPages}
           onClick={() => onPageChange(currentPage + 1)}
         >
-          &raquo;
+          &rsaquo;
         </button>
       </div>
     </div>
